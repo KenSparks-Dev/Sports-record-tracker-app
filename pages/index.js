0a8bda4732e3ac8a6cf66ccd7b491e1c6bd97772
Ken Sparks <kensparks.dev@gmail.com>
2023-03-07 13:54:17 -0500

    Add constants file and remove SEASONS array from index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from '@/styles/Home.module.css'
 import {useState, useEffect} from 'react'
 import PlayerInfo from './components/PlayerInfo'
 import SeasonSelector from './components/SeasonSelector'
+import {SEASONS} from './constants'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -13,88 +14,6 @@ export default function Home() {
   const [player, setPlayer] = useState('')
   const [loading, setLoading] = useState(true)
   const [season, setSeason] = useState('2022')
-  const SEASONS = [
-    { 
-      season: '2022-2023', 
-      year: '2022'
-    }, 
-    { 
-      season: '2021-2022', 
-      year: '2021'
-    },
-    { 
-      season: '2020-2021', 
-      year: '2020'
-    },
-    { 
-      season: '2019-2020', 
-      year: '2019'
-    },
-    { 
-      season: '2018-2019', 
-      year: '2018'
-    }, 
-    { 
-      season: '2017-2018', 
-      year: '2017'
-    },
-    { 
-      season: '2016-2017', 
-      year: '2016'
-    },
-    { 
-      season: '2015-2016', 
-      year: '2015'
-    },
-    { 
-      season: '2014-2015', 
-      year: '2014'
-    },
-    { 
-      season: '2013-2014', 
-      year: '2013'
-    }, 
-    { 
-      season: '2012-2013', 
-      year: '2012'
-    },
-    { 
-      season: '2011-2012', 
-      year: '2011'
-    },
-    { 
-      season: '2010-2011', 
-      year: '2010'
-    },
-    { 
-      season: '2009-2010', 
-      year: '2009'
-    },
-    { 
-      season: '2008-2009', 
-      year: '2008'
-    },
-    { 
-      season: '2007-2008', 
-      year: '2007'
-    },
-    { 
-      season: '2006-2007', 
-      year: '2006'
-    },
-    { 
-      season: '2005-2014', 
-      year: '2005'
-    }, 
-    { 
-      season: '2004-2005', 
-      year: '2004'
-    },
-    { 
-      season: '2003-2004', 
-      year: '2003'
-    },
-    ]
 
   //Use Effect
    useEffect(() => {
