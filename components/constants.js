a1c225bd1153bc9052c42786a063887130140582
Ken Sparks <kensparks.dev@gmail.com>
2023-04-12 14:38:45 -0400

    add bgColor and ImageURL to Seasons array

--- components/constants.js
+++ components/constants.js
@@ -3,82 +3,122 @@ export const SEASONS = [
     { 
       season: '2022-2023', 
       year: '2022',
+      bgColor: 'purple',
+      imageUrl: '/LA-lebron.png'
     }, 
     { 
       season: '2021-2022', 
-      year: '2021'
+      year: '2021',
+      bgColor: 'purple',
+      imageUrl: '/LA-lebron.png'    
     },
     { 
       season: '2020-2021', 
-      year: '2020'
+      year: '2020',
+      bgColor: 'purple',
+      imageUrl: '/LA-lebron.png'    
     },
     { 
       season: '2019-2020', 
-      year: '2019'
+      year: '2019',
+      bgColor: 'purple',
+      imageUrl: '/LA-lebron.png'    
     },
     { 
       season: '2018-2019', 
-      year: '2018'
+      year: '2018',
+      bgColor: 'navy',
+      imageUrl:  '/cleveland-bron-2.jpeg'   
     }, 
     { 
       season: '2017-2018', 
-      year: '2017'
+      year: '2017',
+      bgColor: 'navy',
+      imageUrl:  '/cleveland-bron-2.jpeg'   
     },
     { 
       season: '2016-2017', 
-      year: '2016'
+      year: '2016',
+      bgColor: 'navy',
+      imageUrl:  '/cleveland-bron-2.jpeg'   
     },
     { 
       season: '2015-2016', 
-      year: '2015'
+      year: '2015',
+      bgColor: 'navy',
+      imageUrl:  '/cleveland-bron-2.jpeg'   
     },
     { 
       season: '2014-2015', 
-      year: '2014'
+      year: '2014',
+      bgColor: 'red',
+      imageUrl: '/miami-bron.png'    
     },
     { 
       season: '2013-2014', 
-      year: '2013'
+      year: '2013',
+      bgColor: 'red',
+      imageUrl: '/miami-bron.png'    
     }, 
     { 
       season: '2012-2013', 
-      year: '2012'
+      year: '2012',
+      bgColor: 'red',
+      imageUrl: '/miami-bron.png'    
     },
     { 
       season: '2011-2012', 
-      year: '2011'
+      year: '2011',
+      bgColor: 'red',
+      imageUrl: '/miami-bron.png'    
     },
     { 
       season: '2010-2011', 
-      year: '2010'
+      year: '2010',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
       season: '2009-2010', 
-      year: '2009'
+      year: '2009',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
       season: '2008-2009', 
-      year: '2008'
+      year: '2008',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
       season: '2007-2008', 
-      year: '2007'
+      year: '2007',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
       season: '2006-2007', 
-      year: '2006'
+      year: '2006',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
-      season: '2005-2014', 
-      year: '2005'
+      season: '2005-2016', 
+      year: '2005',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     }, 
     { 
       season: '2004-2005', 
-      year: '2004'
+      year: '2004',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     { 
       season: '2003-2004', 
-      year: '2003'
+      year: '2003',
+      bgColor: 'navy',
+      imageUrl: '/cleveland-bron-1.jpeg'    
     },
     ]
 
